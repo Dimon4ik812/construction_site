c3cb51b23f4f45081ea45fbcc6ffd0b547561db3
macbook <kriukov@pos-credir.ru>
2025-04-24 16:32:57 +0300

    изменение модели услуги

--- construction/models.py
+++ construction/models.py
@@ -4,10 +4,22 @@ from django.db import models
 class Services(models.Model):
     """Модель услуг"""
 
+    SELECT_UNIT = [
+        ("hour", "час"),
+        ("square meter", "кв/м"),
+    ]
+
     name = models.CharField(max_length=100, verbose_name="название услуги")
     description = models.CharField(max_length=500, verbose_name="описание услуги")
     price = models.IntegerField(verbose_name="цена")
     image = models.ImageField(upload_to="images/", blank=True, null=True)
+    unit_of_measurement = models.CharField(
+        max_length=16,
+        choices=SELECT_UNIT,
+        verbose_name="Единица измерения",
+        help_text="Выберите единицу измерения",
+        default="hour",
+    )
 
     def __str__(self):
         return f" {self.name} - {self.description} - {self.price}"
